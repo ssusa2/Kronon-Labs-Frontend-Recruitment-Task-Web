dc7542b67c8cd95dbe20f0142676ba2245f08eed
ssusa2 <audgns9207@naver.com>
2025-01-12 12:36:45 +0900

    refactor: rename navbar to nav-bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import './globals.css'
 import Head from 'next/head'
-import Navbar from '@/components/navbar/Navbar'
+import Navbar from '@/components/nav-bar/Navbar'
 
 export const metadata: Metadata = {
   title: 'Kronon Labs Frontend Recruitment Task',
