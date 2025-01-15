53dc42a6a8ca3cb0306e3e7e2f4da92d297612be
ssusa2 <audgns9207@naver.com>
2025-01-15 12:22:44 +0900

    fix: recoil state 폴더수정

--- src/state/orderBook.ts
+++
@@ -1,9 +0,0 @@
-'use client'
-
-import { OrderBookItemType } from '@/app/en/trade/BTCUSDT/_component/OrderBook'
-import { atom } from 'recoil'
-
-export const orderBookItemState = atom<OrderBookItemType | null>({
-  key: 'orderBookItemState',
-  default: null,
-})
